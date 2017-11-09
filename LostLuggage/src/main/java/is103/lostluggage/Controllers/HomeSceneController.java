100c27ac78b085873e65feb400ae103b2ad709a2
Thijs Zijdel <Thijs.zijdel@gmail.com>
2017-11-09 12:10:21 +0100

    Testing2

--- LostLuggage/src/main/java/is103/lostluggage/Controllers/HomeSceneController.java
+++ LostLuggage/src/main/java/is103/lostluggage/Controllers/HomeSceneController.java
@@ -20,9 +20,13 @@ public class HomeSceneController implements Initializable {
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me ggggggg!");
+        System.out.println("You clicked me!");
+        System.out.println("Changed this for testing");
+        System.out.println("second branch?");
     }
 
+    public HomeSceneController() {
+    }
     /**
      * Initializes the controller class.
      */
