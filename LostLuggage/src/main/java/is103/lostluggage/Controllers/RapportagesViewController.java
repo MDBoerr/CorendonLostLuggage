690b35a80548ecc9ad300ebfd181aab5e42c2898
Daron <daronozdemir@gmail.com>
2017-11-13 11:12:16 +0100

    new rapportages files

---
+++ LostLuggage/src/main/java/is103/lostluggage/Controllers/RapportagesViewController.java
@@ -0,0 +1,22 @@
+package is103.lostluggage.Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author daron
+ */
+public class RapportagesViewController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
