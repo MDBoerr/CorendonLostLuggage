f466208e0dcecd44779caade38cff5ed8c1211cd
Daron <daronozdemir@gmail.com>
2017-11-13 15:32:43 +0100

    Manager links via managerhome

---
+++ LostLuggage/src/main/java/is103/lostluggage/Controllers/ManagerHomeViewController.java
@@ -0,0 +1,89 @@
+package is103.lostluggage.Controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author daron
+ */
+public class ManagerHomeViewController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    
+    
+    @FXML 
+    protected void rapportageView(ActionEvent event) throws IOException {
+        try { 
+            Parent root1 = FXMLLoader.load(getClass().getResource("/Views/ManagerRapportageView.fxml"));
+            //Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+
+        
+        } catch (IOException ex) {
+            Logger.getLogger(HomeViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+        @FXML 
+    protected void naarGevondenOverzichtScherm(ActionEvent event) throws IOException {
+        try { 
+            Parent root1 = FXMLLoader.load(getClass().getResource("/Views/ManagerGevondenView.fxml"));
+            //Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        
+        } catch (IOException ex) {
+            Logger.getLogger(HomeViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+        @FXML 
+    protected void naarVermisteOverzichtScherm(ActionEvent event) throws IOException {
+        try { 
+            Parent root1 = FXMLLoader.load(getClass().getResource("/Views/ManagerVerlorenView.fxml"));
+            //Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        
+        } catch (IOException ex) {
+            Logger.getLogger(HomeViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+        @FXML 
+    protected void naarTeruggebracht(ActionEvent event) throws IOException {
+        try { 
+            Parent root1 = FXMLLoader.load(getClass().getResource("/Views/ManagerTerugView.fxml"));
+            //Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        
+        } catch (IOException ex) {
+            Logger.getLogger(HomeViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ LostLuggage/src/main/java/is103/lostluggage/Controllers/ManagerVerlorenViewController.java
@@ -0,0 +1,44 @@
+package is103.lostluggage.Controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author daron
+ */
+public class ManagerVerlorenViewController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+ 
+            @FXML 
+    protected void backHomeButton(ActionEvent event) throws IOException {
+        try { 
+            Parent root1 = FXMLLoader.load(getClass().getResource("/Views/ManagerHomeView.fxml"));
+            //Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        
+        } catch (IOException ex) {
+            Logger.getLogger(HomeViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
