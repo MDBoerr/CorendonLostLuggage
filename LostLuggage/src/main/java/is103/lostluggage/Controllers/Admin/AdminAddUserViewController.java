c02b2c5e1fea178563cf35cc03143e51615664c3
Arthur <arthurkrom@hotmail.com>
2017-11-24 22:04:39 +0100

    Namen van labels in serviceinvoer aangepast, adminadduser uitgebreid.

--- LostLuggage/src/main/java/is103/lostluggage/Controllers/Admin/AdminAddUserViewController.java
+++ LostLuggage/src/main/java/is103/lostluggage/Controllers/Admin/AdminAddUserViewController.java
@@ -16,6 +16,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
 /**
  * FXML Controller class
@@ -29,8 +34,27 @@ public class AdminAddUserViewController implements Initializable {
      * Initializes the controller class.
      */
     @FXML
-    private Button backBtn;
-    
+    private Label firstnameLbl;
+
+    @FXML
+    //This label displays an errors
+    private Label errorMessageLbl;
+
+    @FXML
+    private Button addUserBtn;
+
+    @FXML
+    private TextField firstnameField;
+
+    @FXML
+    private TextField surnameField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private ChoiceBox roleChoiceBox;
+
     private String header = "Voeg een Gebruiker toe";
 
     @Override
@@ -40,13 +64,59 @@ public class AdminAddUserViewController implements Initializable {
         } catch (IOException ex) {
             Logger.getLogger(OverviewUserController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        //To Previous Scene
+
+        //Set which view was previous
         MainViewController.previousView = "/Views/HomeUserView.fxml";
+
+        //Add options to choicebox
+        roleChoiceBox.getItems().addAll("Administrator", "Manager", "Service Employee");
+
+        //Default value is set to Service Employee as Administrator will most likely add a user with that role.
+        roleChoiceBox.setValue("Service Employee");
     }
 
     @FXML
     protected void backHomeUserView(ActionEvent event) throws IOException {
         MainApp.switchView("/Views/HomeUserView.fxml");
+    }
+
+    @FXML
+    //This method allows the administrator to add a new user to the system
+    /*
+    @TODO
+    Verify the entered info (amount of chars, weird chars, etc)
+    Enter username/pw or does the system generate it?
+    Magic numbers
+     */
+    public void addUser(ActionEvent event) {
+
+        //default error message is empty
+        String errorMessage = "";
+
+        String firstname = firstnameField.getText();
+
+        if (firstname.length() < 4) {
+            errorMessage += "Firstname must contain atleast 4 characters\n";
+        }
+
+        String surname = surnameField.getText();
+
+        if (surname.length() < 4) {
+            errorMessage += ("Surname must contain atleast 4 characters\n");
+        }
+
+        String password = passwordField.getText();
+
+        if (password.length() < 6) {
+            errorMessage += ("Password must contain atleast 6 characters ");
+        }
+        
+        //Put the error message on the label
+        errorMessageLbl.setText(errorMessage);
+
+        
+        String role = roleChoiceBox.getValue().toString();
+
     }
 
 }
