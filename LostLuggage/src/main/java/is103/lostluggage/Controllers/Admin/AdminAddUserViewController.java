9082bb8638413af97144bcda97aba04233722068
Michael  de Boer <michael-deboer@live.nl>
2017-11-26 23:39:39 +0100

    Minor changes to AddUser

--- LostLuggage/src/main/java/is103/lostluggage/Controllers/Admin/AdminAddUserViewController.java
+++ LostLuggage/src/main/java/is103/lostluggage/Controllers/Admin/AdminAddUserViewController.java
@@ -6,9 +6,12 @@
 package is103.lostluggage.Controllers.Admin;
 
 import is103.lostluggage.Controllers.MainViewController;
+import is103.lostluggage.Database.MyJDBC;
 import is103.lostluggage.MainApp;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,6 +19,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -30,8 +34,14 @@ public class AdminAddUserViewController implements Initializable {
      */
     @FXML
     private Button backBtn;
-    
+
     private String header = "Voeg een Gebruiker toe";
+
+    @FXML
+    private TextField userNameField;
+
+    @FXML
+    private TextField passwordField;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -45,8 +55,27 @@ public class AdminAddUserViewController implements Initializable {
     }
 
     @FXML
-    protected void backHomeUserView(ActionEvent event) throws IOException {
-        MainApp.switchView("/Views/HomeUserView.fxml");
+    protected void backHomeUserView(ActionEvent event) throws IOException, SQLException {
+
+        String userName = userNameField.getText();
+        String userPassword = passwordField.getText();
+
+        System.out.println("username: " + userName + "password: " + userPassword);
+
+        MyJDBC db = new MyJDBC("AirlineDemo");
+
+//        ResultSet resultSet;
+//
+//        resultSet = db.executeResultSetQuery("SELECT IATACode, Name, TimeZone FROM Airport");
+        try {
+
+            String query = String.format("INSERT INTO Airport(`IATACode` , `Name` , `TimeZone` ) VALUES (%s, %s, 2)", userName, userPassword);
+            int result;
+            result = db.executeUpdateQuery(query);
+        } catch (Exception e) {
+        }
+
+        MainApp.switchView("/Views/UserScene.fxml");
     }
 
 }
