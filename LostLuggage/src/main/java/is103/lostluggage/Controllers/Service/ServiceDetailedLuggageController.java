60829b006e3a512e36a1d3313c237c377edca7e1
Thijs Zijdel <Thijs.zijdel@gmail.com>
2017-12-05 12:13:10 +0100

    Service Detailed luggage - minor changes

--- LostLuggage/src/main/java/is103/lostluggage/Controllers/Service/ServiceDetailedLuggageController.java
+++ LostLuggage/src/main/java/is103/lostluggage/Controllers/Service/ServiceDetailedLuggageController.java
@@ -66,28 +66,32 @@ import javafx.util.Duration;
  */
 public class ServiceDetailedLuggageController implements Initializable {
     
-    @FXML
-    public Label id_field;
     
-    @FXML
-    private JFXTextField idField;
+    public String language = "English";
     
-    @FXML
-    private JFXTextField typeField;
+    @FXML private JFXTextField registrationNr;
+    @FXML private JFXTextField luggageTag;
+    @FXML private JFXTextField type;
+    @FXML private JFXTextField brand;
+    @FXML private JFXTextField mainColor;
+    @FXML private JFXTextField secondColor;
+    @FXML private JFXTextField size;
+    @FXML private JFXTextField weight;    
+    @FXML private JFXTextArea signatures;
 
-    @FXML  
-    private JFXTextField brandField;
+    @FXML private JFXTextField passengerId;
+    @FXML private JFXTextField passangerName;
+    @FXML private JFXTextField address;        
+    @FXML private JFXTextField place;
+    @FXML private JFXTextField postalCode;
+    @FXML private JFXTextField country;
+    @FXML private JFXTextField email;   
+    @FXML private JFXTextField phone;   
     
-    @FXML  
-    private JFXTextField colorField;
-    
-    @FXML  
-    private JFXTextArea signaturesField;
-    
-    @FXML
-    //Field that contains the employeeId
-    private JFXTextField employeeIdField;
-    
+    @FXML private JFXTextField timeFound;
+    @FXML private JFXTextField dateFound;
+    @FXML private JFXTextField locationFound;
+    @FXML private JFXTextField flight;
     
 
     
@@ -111,7 +115,7 @@ public class ServiceDetailedLuggageController implements Initializable {
 
     
     
-    
+    @FXML
     private void initializeFoundFields() throws SQLException{
         String id = LuggageDetails.getInstance().currentLuggage().getRegistrationNr();
         System.out.println("iD: "+id);
@@ -120,97 +124,131 @@ public class ServiceDetailedLuggageController implements Initializable {
             ResultSet resultSet = db.executeResultSetQuery("SELECT * FROM foundLuggage WHERE registrationNr='"+id+"'");
                 
             while (resultSet.next()) {
-                //String registrationNr =     resultSet.getString("registrationNr");
-                //String dateFound =          resultSet.getString("dateFound");
-                //String timeFound =          resultSet.getString("timeFound");
+                String getRegistrationNr =     resultSet.getString("registrationNr");
+                String getDateFound =          resultSet.getString("dateFound");
+                String getTimeFound =          resultSet.getString("timeFound");
                 
-                //String luggageTag =         resultSet.getString("luggageTag");
-                int luggageType =           resultSet.getInt("luggageType");
-                String brand =              resultSet.getString("brand");
-                int mainColor =             resultSet.getInt("mainColor");
-                int secondColor =           resultSet.getInt("secondColor");
-                //int size =                  resultSet.getInt("size");
-                //int weight =                resultSet.getInt("weight");   
-                String otherCharacteristics=resultSet.getString("otherCharacteristics");
-                int passengerId =           resultSet.getInt("passengerId");
+                String getLuggageTag =         resultSet.getString("luggageTag");
+                int getLuggageType =           resultSet.getInt("luggageType");
+                String getBrand =              resultSet.getString("brand");
+                int getMainColor =             resultSet.getInt("mainColor");
+                int getSecondColor =           resultSet.getInt("secondColor");
+                int getSize =                  resultSet.getInt("size");
+                int getWeight =                resultSet.getInt("weight");   
+                String getOtherCharacteristics=resultSet.getString("otherCharacteristics");
+                int getPassengerId =           resultSet.getInt("passengerId");
                 
-                //String arrivedWithFlight =  resultSet.getString("arrivedWithFlight"); 
-                int locationFound =         resultSet.getInt("locationFound");
+                String getFlight =              resultSet.getString("arrivedWithFlight"); 
+                int getLocationFound =         resultSet.getInt("locationFound");
                 //String employeeId =         resultSet.getString("employeeId");
                 //int matchedId =              resultSet.getInt("matchedId");
  
-            idField.setText(id);  
-//            setType(db,luggageType);
-            brandField.setText(brand);
+            registrationNr.setText(getRegistrationNr);  
+            setType(db,getLuggageType);
+            brand.setText(getBrand);
             
+            dateFound.setText(getDateFound);
+            timeFound.setText(getTimeFound);
+            luggageTag.setText(getLuggageTag);
             
+            setColor(db, getMainColor); 
+            setSecondColor(db, getSecondColor);
             
-//            setColor(db, mainColor); 
-//            setSecondColor(db, secondColor);
+            signatures.setText(getOtherCharacteristics);
             
-            signaturesField.setText(otherCharacteristics);
+            String setSize = Integer.toString(getSize);
+            size.setText(setSize);
             
-//            setPassenger(db, passengerId);
-//            
-//            setLocation(db, locationFound);
+            String setWeight = Integer.toString(getWeight);
+            weight.setText(setWeight);
+            
+            flight.setText(getFlight);
+            setPassenger(db, getPassengerId);
+            
+            setLocation(db, getLocationFound);
             }
         
     }
     
+    @FXML
     private void setColor(MyJDBC db, int colorD) throws SQLException {
         ResultSet result_color = db.executeResultSetQuery("SELECT * FROM color WHERE ralCode='"+colorD+"'");
         while (result_color.next()) {    
-            String color = result_color.getString("english");
-            colorField.setText(color);
+            String color = result_color.getString(language);
+            mainColor.setText(color);
         }
     }
     private void setType(MyJDBC db, int luggageType) throws SQLException {
         ResultSet result_type = db.executeResultSetQuery("SELECT * FROM luggagetype WHERE luggageTypeId='"+luggageType+"'");
         while (result_type.next()) {    
-            String type = result_type.getString("english");
-            typeField.setText(type);
+            String typeGotten = result_type.getString(language);
+            type.setText(typeGotten);
         }
     }
     
-    private void setSecondColor(MyJDBC db, int secondColor) {
-//        ResultSet result_second = db.executeResultSetQuery("SELECT * FROM color WHERE ralCode='"+secondColor+"'");
-//        while (result_second.next()) {    
-//            String color = result_second.getString("english");
-//            colorField2.setText(color);
-//        }
+    private void setSecondColor(MyJDBC db, int secondColor2) throws SQLException {
+        ResultSet result_second = db.executeResultSetQuery("SELECT * FROM color WHERE ralCode='"+secondColor2+"'");
+        while (result_second.next()) {    
+            String color = result_second.getString(language);
+            secondColor.setText(color);
+        }
     }     
 
-    private void setPassenger(MyJDBC db, int passengerId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void setPassenger(MyJDBC db, int passengerIdG) throws SQLException {
+        ResultSet result_second = db.executeResultSetQuery("SELECT * FROM passenger WHERE passengerId='"+passengerIdG+"'");
+        while (result_second.next()) {    
+            String idG = result_second.getString("passengerId");
+            String nameG = result_second.getString("name");
+            String addressG = result_second.getString("address");
+            String placeG = result_second.getString("place");
+            String postalCodeG = result_second.getString("postalCode");
+            
+            String countryG = result_second.getString("country");
+            String emailG = result_second.getString("email");
+            String phoneG = result_second.getString("phone");
+            
+            passengerId.setText(idG);
+            passangerName.setText(nameG);
+            address.setText(addressG);
+            place.setText(placeG);
+            postalCode.setText(postalCodeG);
+            country.setText(countryG);
+            email.setText(emailG);
+            phone.setText(phoneG);
+        }
     }
 
-    private void setLocation(MyJDBC db, int locationFound) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void setLocation(MyJDBC db, int locationFoundG) throws SQLException {
+        ResultSet result = db.executeResultSetQuery("SELECT * FROM color WHERE ralCode='"+locationFoundG+"'");
+        while (result.next()) {    
+            String location = result.getString(language);
+            locationFound.setText(location);
+        }   
     }
    
     @FXML
     protected void saveLuggageChanges(ActionEvent event) throws SQLException {
-        String luggageId = idField.getText();
-        String luggageType = typeField.getText();
-        String luggageBrand = brandField.getText();
-        String luggageColor = colorField.getText();
-        String luggageSignatures = signaturesField.getText();
-        
-        
-        MyJDBC db = MainApp.connectToDatabase();
-        ResultSet resultSet;
-        resultSet = db.executeResultSetQuery("SELECT * FROM foundLuggage WHERE idfoundLuggage='"+luggageId+"'");
-        System.out.println("result is:"+resultSet);
-        if (    luggageType == null || "".equals(luggageType) ||
-                luggageBrand == null || "".equals(luggageBrand) ||
-                luggageColor == null || "".equals(luggageColor) ||
-                luggageSignatures == null || "".equals(luggageSignatures)
-                ) {
-            System.out.println("Een van de velden is leeg of null");
-        } else {
-            db.executeUpdateQuery("UPDATE `LostLuggage`.`foundLuggage` SET `type`='"+luggageType+"', `brand`='"+luggageBrand+"', `color`='"+luggageColor+"', `signatures`='"+luggageSignatures+"' WHERE `idfoundLuggage`='"+luggageId+"'");
-            System.out.println("DB row is updated!");
-        }
+//        String luggageId = idField.getText();
+//        String luggageType = typeField.getText();
+//        String luggageBrand = brandField.getText();
+//        String luggageColor = colorField.getText();
+//        String luggageSignatures = signaturesField.getText();
+//        
+//        
+//        MyJDBC db = MainApp.connectToDatabase();
+//        ResultSet resultSet;
+//        resultSet = db.executeResultSetQuery("SELECT * FROM foundLuggage WHERE idfoundLuggage='"+luggageId+"'");
+//        System.out.println("result is:"+resultSet);
+//        if (    luggageType == null || "".equals(luggageType) ||
+//                luggageBrand == null || "".equals(luggageBrand) ||
+//                luggageColor == null || "".equals(luggageColor) ||
+//                luggageSignatures == null || "".equals(luggageSignatures)
+//                ) {
+//            System.out.println("Een van de velden is leeg of null");
+//        } else {
+//            db.executeUpdateQuery("UPDATE `LostLuggage`.`foundLuggage` SET `type`='"+luggageType+"', `brand`='"+luggageBrand+"', `color`='"+luggageColor+"', `signatures`='"+luggageSignatures+"' WHERE `idfoundLuggage`='"+luggageId+"'");
+//            System.out.println("DB row is updated!");
+//        }
    
     }
 
