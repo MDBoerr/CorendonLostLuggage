b0a5e56265958232715898132723546497e9a06f
Thijs Zijdel <Thijs.zijdel@gmail.com>
2018-01-15 21:27:03 +0100

    V2 Languages   <<<<<<<<<

--- LostLuggage/src/main/java/is103/lostluggage/Controllers/Service/ServiceHomeViewController.java
+++ LostLuggage/src/main/java/is103/lostluggage/Controllers/Service/ServiceHomeViewController.java
@@ -5,6 +5,7 @@ import com.sun.org.apache.xerces.internal.xs.StringList;
 import is103.lostluggage.Controllers.MainViewController;
 import is103.lostluggage.MainApp;
 import is103.lostluggage.Model.Service.Data.ServiceDataMatch;
+import is103.lostluggage.Model.settings;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -37,37 +38,21 @@ public class ServiceHomeViewController implements Initializable {
                                                 //for: manual matching
     //index of language strings
     private final int TITLE = 0, BUT_FOUND = 1, BUT_LOST = 2, BUT_MATCH = 3, BUT_INPUT = 4;
-    private final int AMOUNT_OF_LANGUAGE_FIELDS = 4;
     
-    private final String[] languageArray = new String[AMOUNT_OF_LANGUAGE_FIELDS+1];
     @FXML private JFXButton button_found, button_lost, button_match, button_input;
     
-    private void initializeLanguageStrings(){
-        if(MainApp.getLanguage().equals("dutch")) {
-            //set al the data for the getLanguageFor string array
-            languageArray[TITLE]     = "Service home pagina";
-            languageArray[BUT_FOUND] = "Gevonden bagage overzicht";
-            languageArray[BUT_LOST]  = "Verloren bagage overzicht";
-            languageArray[BUT_MATCH] = "Overeenkomende Bagage >";
-            languageArray[BUT_INPUT] = "Bagage invoeren +";
-        } else if (MainApp.getLanguage().equals("english")) {
-            languageArray[TITLE]     = "Service Home";
-            languageArray[BUT_FOUND] = "Overview Found Luggage";
-            languageArray[BUT_LOST]  = "Overview Lost Luggage";
-            languageArray[BUT_MATCH] = "Luggage Matching >";
-            languageArray[BUT_INPUT] = "Input Luggage +";
-        }
-    }
-    private String getLanguageFor(int index){
-        return languageArray[index];
-    }
-   
+
+//    private String getLanguageFor(int index){
+//        return languageArray[index];
+//    }
+   private String[][] languages = settings.serviceHomeLanguage();
     private void setRightLanguage(){
-       initializeLanguageStrings();
-       button_found.setText(getLanguageFor(BUT_FOUND));
-       button_lost.setText( getLanguageFor(BUT_LOST ));
-       button_match.setText(getLanguageFor(BUT_MATCH));
-       button_input.setText(getLanguageFor(BUT_INPUT));
+       
+       
+       button_found.setText( settings.getLanguageFor(BUT_FOUND, languages) );
+       button_lost.setText(  settings.getLanguageFor(BUT_LOST,  languages) );
+       button_match.setText( settings.getLanguageFor(BUT_MATCH, languages) );
+       button_input.setText( settings.getLanguageFor(BUT_INPUT, languages) );
    }
           
             
@@ -140,7 +125,7 @@ public class ServiceHomeViewController implements Initializable {
         
         //set the view's title
         try {
-            MainViewController.getInstance().getTitle(getLanguageFor(TITLE));
+            MainViewController.getInstance().getTitle( settings.getLanguageFor(TITLE, languages) );
         } catch (IOException ex) {
             Logger.getLogger(ServiceHomeViewController.class.getName()).log(Level.SEVERE, null, ex);
         }  
